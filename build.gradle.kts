909287cd860fb4fe729ed82e7b4594ed571c25c8
NanoBotAgent <nanobotaiagent@gmail.com>
2026-05-02 02:28:23 +0200

    build: Gradle build with Paper 26.1.2 + WorldEdit 7.4.3-beta-01

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("java")
-    id("com.gradleup.shadow") version "9.0.0-beta17"
+    java
+    id("com.github.johnrengelman.shadow") version "8.1.8"
 }
 
 group = "com.nomadsmp"
@@ -14,38 +14,21 @@ java {
 
 repositories {
     mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/") {
-        name = "PaperMC"
-    }
-    maven("https://mvn.intellectualsites.com/content/repositories/snapshots/") {
-        name = "IntellectualSites"
-    }
-    maven("https://maven.enginehub.org/repo/") {
-        name = "EngineHub"
-    }
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://maven.enginehub.org/repo/")
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:26.1.2.build.53-stable")
-    compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Bukkit:2.15.0")
-    compileOnly("com.sk89q.worldedit:worldedit-core:7.4.2")
-}
-
-tasks.jar {
-    enabled = false
+    compileOnly("io.papermc.paper:paper-api:26.1.2-R0.1-SNAPSHOT")
+    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.4.3-beta-01")
+    compileOnly("com.sk89q.worldedit:worldedit-core:7.4.3-beta-01")
 }
 
 tasks.shadowJar {
-    archiveClassifier = ""
+    archiveClassifier.set("")
     minimize()
 }
 
 tasks.build {
     dependsOn(tasks.shadowJar)
-}
-
-tasks.processResources {
-    filesMatching("plugin.yml") {
-        expand("version" to version)
-    }
 }
