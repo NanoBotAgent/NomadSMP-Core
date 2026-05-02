d67367ff680f316e33d1a3ce598d89622403466a
NanoBotAgent <nanobotaiagent@gmail.com>
2026-05-02 02:31:51 +0200

    feat: main plugin class with module lifecycle

--- src/main/java/com/nomadsmp/core/NomadCore.java
+++ src/main/java/com/nomadsmp/core/NomadCore.java
@@ -1,9 +1,9 @@
 package com.nomadsmp.core;
 
-import com.nomadsmp.core.config.ConfigManager;
-import com.nomadsmp.core.modules.*;
-import com.nomadsmp.core.listeners.*;
 import com.nomadsmp.core.commands.NomadCommand;
+import com.nomadsmp.core.config.ConfigManager;
+import com.nomadsmp.core.listeners.*;
+import com.nomadsmp.core.modules.*;
 import com.nomadsmp.core.utils.HomeStorage;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -13,6 +13,8 @@ public class NomadCore extends JavaPlugin {
     private static NomadCore instance;
     private ConfigManager configManager;
     private HomeStorage homeStorage;
+
+    // Modules
     private NomadModule nomadModule;
     private DailyBuffModule dailyBuffModule;
     private ProgressionLockModule progressionLockModule;
@@ -22,47 +24,75 @@ public class NomadCore extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
+
+        // Save default config
         saveDefaultConfig();
         configManager = new ConfigManager(this);
+        configManager.load();
+
+        // Initialize home storage
         homeStorage = new HomeStorage(this);
 
+        // Initialize modules
         nomadModule = new NomadModule(this);
         dailyBuffModule = new DailyBuffModule(this);
         progressionLockModule = new ProgressionLockModule(this);
         antiCheatModule = new AntiCheatModule(this);
         socialModule = new SocialModule(this);
 
-        if (configManager.isNomadEnabled()) nomadModule.enable();
-        if (configManager.isDailyBuffsEnabled()) dailyBuffModule.enable();
-        if (configManager.isProgressionLockEnabled()) progressionLockModule.enable();
-        if (configManager.isAntiCheatEnabled()) antiCheatModule.enable();
-        if (configManager.isSocialEnabled()) socialModule.enable();
+        // Enable modules
+        if (configManager.isNomadSystemEnabled()) {
+            nomadModule.enable();
+        }
+        if (configManager.isDailyBuffsEnabled()) {
+            dailyBuffModule.enable();
+        }
+        if (configManager.isProgressionLockEnabled()) {
+            progressionLockModule.enable();
+        }
+        if (configManager.isAntiCheatEnabled()) {
+            antiCheatModule.enable();
+        }
+        if (configManager.isSocialEnabled()) {
+            socialModule.enable();
+        }
 
-        getServer().getPluginManager().registerEvents(new BuffListeners(this), this);
-        getServer().getPluginManager().registerEvents(new ProgressionListeners(this), this);
-        getServer().getPluginManager().registerEvents(new AntiCheatListeners(this), this);
-        getServer().getPluginManager().registerEvents(new SocialListeners(this), this);
+        // Register listeners
+        Bukkit.getPluginManager().registerEvents(new BuffListeners(this), this);
+        Bukkit.getPluginManager().registerEvents(new ProgressionListeners(this), this);
+        Bukkit.getPluginManager().registerEvents(new AntiCheatListeners(this), this);
+        Bukkit.getPluginManager().registerEvents(new SocialListeners(this), this);
 
+        // Register commands
         if (getCommand("nomad") != null) {
             getCommand("nomad").setExecutor(new NomadCommand(this));
         }
-        getLogger().info("NomadSMP-Core enabled!");
+
+        getLogger().info("NomadSMP-Core enabled! All modules loaded.");
     }
 
     @Override
     public void onDisable() {
-        if (nomadModule != null) nomadModule.disable();
         if (dailyBuffModule != null) dailyBuffModule.disable();
-        if (homeStorage != null) homeStorage.save();
+        if (nomadModule != null) nomadModule.disable();
         getLogger().info("NomadSMP-Core disabled.");
     }
 
-    public static NomadCore inst() { return instance; }
-    public ConfigManager cfg() { return configManager; }
-    public HomeStorage homes() { return homeStorage; }
-    public NomadModule nomad() { return nomadModule; }
-    public DailyBuffModule buffs() { return dailyBuffModule; }
-    public ProgressionLockModule prog() { return progressionLockModule; }
-    public AntiCheatModule anticheat() { return antiCheatModule; }
-    public SocialModule social() { return socialModule; }
+    public void reload() {
+        reloadConfig();
+        configManager.load();
+        // Re-apply border, re-schedule tasks
+        if (socialModule != null) socialModule.enable();
+        if (dailyBuffModule != null) dailyBuffModule.updateDailyBuff();
+        getLogger().info("NomadSMP-Core config reloaded.");
+    }
+
+    public static NomadCore getInstance() { return instance; }
+    public ConfigManager getConfigManager() { return configManager; }
+    public HomeStorage getHomeStorage() { return homeStorage; }
+    public NomadModule getNomadModule() { return nomadModule; }
+    public DailyBuffModule getDailyBuffModule() { return dailyBuffModule; }
+    public ProgressionLockModule getProgressionLockModule() { return progressionLockModule; }
+    public AntiCheatModule getAntiCheatModule() { return antiCheatModule; }
+    public SocialModule getSocialModule() { return socialModule; }
 }
