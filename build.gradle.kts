2730839e515b7cc2c633e7d451e6c6bfca5453d1
NanoBotAgent <nanobotaiagent@gmail.com>
2026-05-12 12:40:26 +0200

    chore: bump version to 1.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,34 @@
 plugins {
-    java
-    id("io.github.goooler.shadow") version "8.1.8"
+ java
+ id("io.github.goooler.shadow") version "8.1.8"
 }
 
 group = "com.nomadsmp"
-version = "1.0.0"
+version = "1.0.2"
 
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(25)
-    }
+ toolchain {
+  languageVersion = JavaLanguageVersion.of(25)
+ }
 }
 
 repositories {
-    mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/")
-    maven("https://maven.enginehub.org/repo/")
-    flatDir { dirs("libs") }
+ mavenCentral()
+ maven("https://repo.papermc.io/repository/maven-public/")
+ maven("https://maven.enginehub.org/repo/")
+ flatDir { dirs("libs") }
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:26.1.2.build.53-stable")
-    compileOnly("com.sk89q.worldedit:worldedit-core:7.4.2")
-    compileOnly(":worldedit-bukkit-7.4.3-beta-01")
+ compileOnly("io.papermc.paper:paper-api:26.1.2.build.53-stable")
+ compileOnly("com.sk89q.worldedit:worldedit-core:7.4.2")
+ compileOnly(":worldedit-bukkit-7.4.3-beta-01")
 }
 
 tasks.shadowJar {
-    archiveClassifier.set("")
+ archiveClassifier.set("")
 }
 
 tasks.build {
-    dependsOn(tasks.shadowJar)
+ dependsOn(tasks.shadowJar)
 }
